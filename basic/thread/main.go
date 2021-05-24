97c730f9d4da0c23b30ba7b96f71a406314527d7
vinylrich <whktjd0109@gmail.com>
2021-05-24 23:10:08 +0900

    title: First Study subject: Thread contents: basic of Thread

---
+++ basic/thread/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go fun1()
+	go fun1()
+	for i := 0; i < 20; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("main", i)
+	}
+	fmt.Scanln()
+}
+
+func fun1() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("fun1:", i)
+	}
+}
